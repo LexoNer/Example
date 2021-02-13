76158e2ba0189452fae22468d809c864fe23a0b2
calufin <65758242+calufin@users.noreply.github.com>
2021-02-12 19:39:13 -0600

    actualizacion de menu principal

---
+++ New Unity Project/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interfaces : MonoBehaviour
+{
+    public GameObject Menu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MenuPrincipal()
+    {
+        Menu.SetActive(false);
+    }
+}
